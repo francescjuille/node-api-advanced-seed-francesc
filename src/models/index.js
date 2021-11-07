a3baef676e3cf8dba17eea071fe2ac70b3ef2f3e
Francesc <francescjuille@gmail.com>
2021-11-07 20:07:12 +0100

    feature(mysql): add config anotations to sequelize init file

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,13 @@
-'use strict';
-
+//'use strict';
+const config = require('config')
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+console.log('read mysql config')
+console.log(config.get('mysql'))
+sequelize = new Sequelize(config.get('mysql.host'), config.get('mysql.user'), config.get('mysql.password'), config.get('mysql'));
 
 fs
   .readdirSync(__dirname)
